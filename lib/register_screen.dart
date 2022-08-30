fe3642a9338af6a29472b58d4cd191d187b6ac51
Fadhil Octo <fadhilocto@gmail.com>
2022-08-30 15:22:45 +0700

    cleaning

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:project_simple/login_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
