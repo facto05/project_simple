d4b00d9f4288efa80f0de9a3a2347eb5cec5588e
Fadhil Octo <fadhilocto@gmail.com>
2022-08-30 15:20:58 +0700

    completed register screen and fix splash screen

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:project_simple/login_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -17,22 +18,29 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
   bool _isObscure = true;
+  bool _isObscureConfirm = true;
+
+  @override
+  void initState() {
+    super.initState();
+    referralController.addListener(() {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Register"),
-          backgroundColor: const Color.fromARGB(232, 238, 35, 21),
-        ),
-        body: ListView(
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.all(15),
-              child: Column(children: [
-                const SizedBox(
-                  height: 5,
-                ),
+      appBar: AppBar(
+        title: const Text("Register"),
+        backgroundColor: const Color.fromARGB(232, 155, 15, 15),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(30),
+            child: Column(
+              children: [
                 const Align(
                   alignment: Alignment.topLeft,
                   child: Text(
@@ -133,7 +141,10 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 TextFormField(
                   controller: phoneNumberController,
                   decoration: InputDecoration(
-                      prefixText: "+62 ",
+                      prefixIcon: const Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Text("+62"),
+                      ),
                       hintText: "Input Your Phone Number",
                       enabledBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(15)),
@@ -152,6 +163,12 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   controller: referralController,
                   decoration: InputDecoration(
                       hintText: "Input Your Referral Code",
+                      suffixIcon: (referralController.text.contains("SDK"))
+                          ? SvgPicture.asset(
+                              "svg/icon/ico_data_right.svg",
+                              fit: BoxFit.scaleDown,
+                            )
+                          : null,
                       enabledBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(15)),
                       focusedBorder: OutlineInputBorder(
@@ -191,9 +208,114 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(15))),
                 ),
-              ]),
+                const SizedBox(
+                  height: 10,
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Password must contains :",
+                          style: TextStyle(color: Colors.grey, fontSize: 10),
+                        ),
+                        Text(" \u2022 1 lower case letter [a-z]",
+                            style: TextStyle(color: Colors.grey, fontSize: 10)),
+                        Text(" \u2022 1 Upper case letter [A-Z]",
+                            style: TextStyle(color: Colors.grey, fontSize: 10)),
+                        Text(" \u2022 1 numeric character [0-9]",
+                            style: TextStyle(color: Colors.grey, fontSize: 10)),
+                        Text(" \u2022 at least 10 character in length",
+                            style: TextStyle(color: Colors.grey, fontSize: 10))
+                      ]),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: RichText(
+                    text: const TextSpan(children: <TextSpan>[
+                      TextSpan(text: "Confirm Password"),
+                      TextSpan(text: "*", style: TextStyle(color: Colors.red))
+                    ]),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                TextFormField(
+                  controller: confirmPasswordController,
+                  obscureText: _isObscureConfirm,
+                  decoration: InputDecoration(
+                      hintText: "Input Your Password",
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _isObscureConfirm = !_isObscureConfirm;
+                            });
+                          },
+                          icon: Icon(_isObscureConfirm
+                              ? Icons.visibility_off
+                              : Icons.visibility)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15))),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    String firstName = firstNameController.text.toString();
+                    String lastName = lastNameController.text.toString();
+                    String email = emailController.text.toString();
+                    String phoneNumber = phoneNumberController.text.toString();
+                    String referral = referralController.text.toString();
+                    String password = passwordController.text.toString();
+                    String confirmPassword =
+                        confirmPasswordController.text.toString();
+
+                    print(
+                        "first name => $firstName \nlast name => $lastName \ne-mail => $email \nphone number => $phoneNumber");
+                    print(
+                        "referral => $referral \npassword => $password \nconfirm password => $confirmPassword");
+
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      primary: const Color.fromARGB(232, 155, 15, 15),
+                      minimumSize: const Size.fromHeight(50)),
+                  child: const Text("Submit"),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      side: const BorderSide(
+                          width: 1, color: Color.fromARGB(232, 155, 15, 15)),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      primary: Colors.white,
+                      minimumSize: const Size.fromHeight(50)),
+                  child: const Text(
+                    "Cancel",
+                    style: TextStyle(color: Color.fromARGB(232, 155, 15, 15)),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -14,35 +14,51 @@ class _SplashScreenViewState extends State<SplashScreenView> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 5), () {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => LoginScreen()));
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: Image.asset("/images/bg_splashscreen.png").image,
-              fit: BoxFit.fill)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Image.asset(
-              "/images/logo.png",
-              height: 200,
-              width: 200,
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: Image.asset("images/bg_splashscreen.png").image,
+                fit: BoxFit.fill)),
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Image.asset(
+                      "images/logo.png",
+                      height: 200,
+                      width: 200,
+                    ),
+                  ),
+                  const Text("version 1.0.22",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10,
+                          fontWeight: FontWeight.normal)),
+                ],
+              ),
             ),
-          ),
-          const Text("Copyright @ 2022 by PT. Indocyber Global Teknologi",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
-                  fontWeight: FontWeight.normal)),
-        ],
+            const Text("Copyright @ 2022 by PT. Indocyber Global Teknologi",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                    fontWeight: FontWeight.normal)),
+            const SizedBox(
+              height: 20,
+            )
+          ],
+        ),
       ),
     );
   }
